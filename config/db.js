e393cdeb157ce271d908c8e8c2983b87fb0a791b
akbari-alireza <akbaari.alireza@gmail.com>
2025-09-02 07:52:13 +0430

    first commit

---
+++ config/db.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+
+let cached = global.mongoose
+
+if(!cached) {
+    cached = global.mongoose = {promise: null, conn: null}
+}
+
+async function connectDB() {
+    if(cached.conn) {
+        return cached.conn
+    }
+    if(!cached.promise) {
+        const opts = {
+            bufferCommand: false
+        }
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}`, opts).then((mongoose) => {
+            return mongoose
+        })
+    }
+    cached.conn = await cached.promise
+    return cached.conn
+}
+
+export default connectDB    
